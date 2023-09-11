0a01197bd14a5fe24787a4714a6261d5136dfadb
Yasmine Shakib <shakiby@MacBook-Pro.local>
2023-09-11 11:24:11 +1000

    Markup added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    Homepage
-    </div>
+    <>
+    <header>
+    <img src='Logo.svg'></img>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+    </header>
+   
+    <main></main>
+    <footer></footer>
+    </>
   );
 }
 
